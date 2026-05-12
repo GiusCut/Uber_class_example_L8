65457b0075c47c198a1dd3e47c6284b9cbc41aca
GiusCut <sba24030@student.cct.ie>
2026-05-12 19:02:29 +0100

    Update Uber_class_example_L8.py

--- Uber_class_example_L8.py
+++ Uber_class_example_L8.py
@@ -21,3 +21,8 @@ data_load_state.text("Loading data...done!")
 
 st.subheader("Raw data")
 st.write(data)
+
+
+st.subheader("Num pickups per hour")
+hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
